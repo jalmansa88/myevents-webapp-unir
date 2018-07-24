a8600b6998ed5f05c06ce003c68d5a774be55e45
lordneru <lordneru@gmail.com>
2018-07-24 20:32:32 +0200

    creating develop branch

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginService } from '../../services/login.service';
-import { Router } from '@angular/router';
 import { RoleRouterService } from '../../services/role-router.service';
 
 declare var $;
